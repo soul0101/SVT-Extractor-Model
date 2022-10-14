c16ccfdce68904f589b01900414a796b4387c50d
soul0101 <53980340+soul0101@users.noreply.github.com>
2022-10-15 02:47:52 +0530

    fix bug

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-
 import os
 import numpy as np
 from signver.extractor import MetricExtractor
 
 DIR = os.path.dirname(__file__)
 EXTRACTOR_MODEL_PATH = "models/extractor/metric"
-extractor = MetricExtractor() 
-extractor_model = extractor.load(os.path.join(DIR, EXTRACTOR_MODEL_PATH))
+extractor_model = MetricExtractor() 
+extractor_model.load(os.path.join(DIR, EXTRACTOR_MODEL_PATH))
 
 def extract(image_np):
     return extractor_model.extract(image_np)
