0b1c424ac6e317fde2fc69d6d43e174a0e3a22c5
soul0101 <53980340+soul0101@users.noreply.github.com>
2022-10-17 17:57:47 +0530

    added docstring

--- main.py
+++ main.py
@@ -8,9 +8,35 @@ extractor_model = MetricExtractor()
 extractor_model.load(os.path.join(DIR, EXTRACTOR_MODEL_PATH))
 
 def extract(image_np):
+    """
+    Extracts the features from the given image.
+    
+    Parameters
+    ----------
+    image_np : numpy.ndarray
+        The image to extract features from.
+    
+    Returns
+    -------
+    The extracted features.
+    """
     return extractor_model.extract(image_np)
 
 def signature_feature_extractor(signatures):
+    """
+    This function accepts a batch of signatures in the form of numpy arrays.
+    It extracts features from each signature and returns the feature vectors.
+    
+    Parameters
+    ----------
+    signatures : list or numpy array
+        A batch of signatures in the form of numpy arrays.
+        
+    Returns
+    -------
+    sign_features : numpy array
+        A batch of feature vectors extracted from the signatures.
+    """
     if isinstance(signatures, list):
         sign_features = extract(np.array(signatures) / 255)
     else:
